98eb2cc1add3427bfffb79e8429502b08bb8868b
Otavio <otaviomiranda@usp.br>
2025-10-23 00:30:08 -0300

    wip: primeira tentativa sqlite

--- crawler_al/pipelines.py
+++ crawler_al/pipelines.py
@@ -31,19 +31,32 @@ class UniqueJSONPipeline:
             data_cadastro TEXT NOT NULL,
             disponivel BOOLEAN NOT NULL,
             site TEXT NOT NULL,
-            link TEXT UNIQUE NOT NULL UNIQUE,
+            link TEXT NOT NULL UNIQUE,
             qtd_quartos INTEGER NOT NULL
         )
         """)
 
         self.conexao.commit()
+        #tornar todos os registros do banco como inativos
+        self.cursor.execute(" UPDATE imoveis SET disponivel = 0 WHERE disponivel = 1")
+        self.conexao.commit()
+        
 
     def process_item(self, item, spider):
-        preco = item.get("valorTotal").replace("R$", "").replace(".", "").replace(",", ".").strip()
-        self.cursor.execute("""
-        INSERT INTO imoveis (endereco, preco, data_cadastro, disponivel, site, link, qtd_quartos)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-        """, (item.get("endereco"), preco, date.today().isoformat(), True, "Quinto Andar", item.get("link"), 2))
+        preco = item.get("valorTotal").replace("total", "").replace("R$", "").replace(".", "").replace(",", ".").strip()
+        #se o link do novo item não exisitr no banco, então criar um novo item
+        self.cursor.execute("SELECT * FROM imoveis WHERE link = ?", (item.get("link"),))
+        imovel = self.cursor.fetchone()
+        if imovel is None:
+            self.cursor.execute("""
+            INSERT INTO imoveis (endereco, preco, data_cadastro, disponivel, site, link, qtd_quartos)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+            """, (item.get("endereco"), preco, date.today().isoformat(), 1, "Quinto Andar", item.get("link"), 2))
+        else:
+            #se o link existir, tornar-lo true
+            self.cursor.execute("""
+                UPDATE imoveis SET disponivel = ? WHERE link = ?
+            """, (True, item.get("link")))
         return item
         
     def close_spider(self, spider):
